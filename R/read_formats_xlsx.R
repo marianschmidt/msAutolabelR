c6f5688ee1483d6aeb0246aac1ca946672a56265
marianschmidt <marian.schmidt@gmail.com>
2019-03-11 22:57:27 +0100

    added read_formats_xlsx function

---
+++ R/read_formats_xlsx.R
@@ -0,0 +1,30 @@
+
+#' Import Formats from .xlsx
+#'
+#' Import formats from a predefined xlsx file.
+#'
+#' @param infile_formats file
+#' @param sheet string
+#'
+#' @return dataframe
+#' @export
+
+
+read_formats_xlsx <- function(infile_formats, sheet = NULL) {
+  formats <- readxl::read_xlsx(path = infile_formats,
+                               sheet = sheet)
+
+  #create columns for factor transformation. "var_values" and "var_valnames".
+  a <- formats %>%
+    dplyr::select(dplyr::contains("Value Label ")) %>%
+    colnames() %>%
+    length()
+
+  for(i in 1:a) {
+    formats <- formats %>%
+      tidyr::separate(paste0("Value Label ", i, sep=""), into = c(paste0("var_value",i,sep=""),paste0("var_valname",i,sep="")), sep = "\\=", remove=FALSE)
+  }
+  return(formats)
+
+}
+
--- R/sort_dataframe.R
+++ R/sort_dataframe.R
@@ -10,15 +10,20 @@
 #' @examples
 #' sort_dataframe(iris)
 
-sort_dataframe <- function(df) {
-  sort_vars <- formats %>%
+sort_dataframe <- function(df, formats_df = formats, post_dm = FALSE) {
+  if (post_dm == FALSE) {
+    formats_df <- formats_df %>%
+      dplyr::filter(`Import format` != "not imported")
+  }
+
+  sort_vars <- formats_df %>%
     dplyr::filter(`Sorting Order`> 0) %>%
-    dplyr::arrange(`Sorting Order`)  %>%
+        dplyr::arrange(`Sorting Order`)  %>%
     dplyr::select(`Variable name`) %>%
     as.vector() %>%
     unname() %>%
     unlist()
 
 df <- df %>%
-  dplyr::select(sort_vars, everything())
+  dplyr::select(sort_vars, dplyr::everything())
 }
