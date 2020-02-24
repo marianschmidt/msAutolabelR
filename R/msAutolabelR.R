5b7099e5d8d0841aa43aba8edad845530da4499c
marianschmidt <marian.schmidt@gmail.com>
2020-02-24 11:11:59 +0100

    new global vars set to pass check

---
+++ R/msAutolabelR.R
@@ -0,0 +1,18 @@
+#' msAutolabelR
+#'
+#' Helper Functions to Automatically Label Dataframes Using a Format Table
+#'
+#' @section Main functions:
+#'
+#' Functions include label_values_dataframe, label_vars_dataframe, read_formats, set_missing_values.
+#'
+#' @section Package context:
+#' This package follows the principles of the "tidyverse".
+#'
+#'
+#' @docType package
+#' @name msAutolabelR
+#' @keywords internal
+NULL
+## quiets warnings of R CMD check standard objects that appear in function
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("formats"))
